5d4d61c8d61e1cc656d1409169b50aa254c89b66
Owen <owen@cellarmation.com>
2021-03-11 20:53:44 +0000

    * Created a fresh Unity3D 2019.4.16f1 project * Imported vlc-unity * Changed Api Compatibility Level to ".NET 4.x" * Added example extension methods that clash with the two versions of System.Numerics * Added a basic .gitignore

---
+++ Assets/ExtensionMethods.cs
@@ -0,0 +1,17 @@
+﻿public static class ExtensionMethods
+{
+    public static void Convert(this System.Numerics.Vector3 a, ref UnityEngine.Vector3 b)
+    {
+        b.x = a.X;
+        b.y = a.Y;
+        b.z = -a.Z;
+    }
+
+    public static void Convert(this System.Numerics.Quaternion a, ref UnityEngine.Quaternion b)
+    {
+        b.x = -a.X;
+        b.y = -a.Y;
+        b.z = a.Z;
+        b.w = a.W;
+    }
+}
